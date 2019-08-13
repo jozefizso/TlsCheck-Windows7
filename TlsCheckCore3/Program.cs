87cbe34fde5a8bf2ce70d8aa284f015630c4d493
Jozef Izso <jozef.izso@gmail.com>
2019-08-13 12:21:24 +0200

    Add TlsCheck for .NET Core 3.0.0-preview7

---
+++ TlsCheckCore3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TlsCheckCore3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"Target Framework: Core 3.0.0-preview7");
+                Console.WriteLine($"Environment.Version: {Environment.Version}");
+                Console.WriteLine($"ServicePointManager.SecurityProtocol: {ServicePointManager.SecurityProtocol}");
+
+                var client = new HttpClient();
+                var config = await client.GetStringAsync("https://www.howsmyssl.com/a/check");
+
+                Console.WriteLine();
+                Console.WriteLine(config);
+                Console.WriteLine();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"Exception: {ex.InnerException.Message}");
+                Console.WriteLine($"Exception: {ex.InnerException.InnerException.Message}");
+            }
+
+            Console.WriteLine("Done.");
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
