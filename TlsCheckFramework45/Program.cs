aebd302fcf5b24408eee4d95563867c07655a590
Jozef Izso <jozef.izso@gmail.com>
2019-08-13 09:38:31 +0200

    Add TlsCheck for .NET Framework 4.5

---
+++ TlsCheckFramework45/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TlsCheckFramework45
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"Environment.Version: {Environment.Version}");
+                Console.WriteLine($"ServicePointManager.SecurityProtocol: {ServicePointManager.SecurityProtocol}");
+
+                var client = new HttpClient();
+                var config = await client.GetStringAsync("https://www.howsmyssl.com/a/check");
+
+                Console.WriteLine();
+                Console.WriteLine(config);
+                Console.WriteLine();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"Exception: {ex.InnerException.Message}");
+                Console.WriteLine($"Exception: {ex.InnerException.InnerException.Message}");
+            }
+
+            Console.WriteLine("Done.");
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
